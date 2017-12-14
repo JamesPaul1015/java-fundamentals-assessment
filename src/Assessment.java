19a2000e490843040cb1020efd11a357d482ade0
jamespaul1015 <jamespaul1015@yahoo.com>
2017-12-14 13:59:12 -0600

    Problem 1 average

--- src/Assessment.java
+++ src/Assessment.java
@@ -34,3 +34,22 @@ class Sum
         return x+y;
     }
 }
+
+
+class Average {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[]{10,20,15,25,16,60,100};
+
+
+        int sum = 0;
+
+        for(int i=0; i < numbers.length ; i++)
+            sum = sum + numbers[i];
+
+        double average = sum / numbers.length;
+
+        System.out.println("Average value of array elements is : " + average);
+    }
+}
